c1581f0177f04b8998809b8ec6d173aea1462953
xiaoguo-jia <xiaoguojia@ufl.edu>
2024-06-13 21:21:44 -0400

    added background, added website logo, changed color on navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -6,8 +6,12 @@ const Navbar = () => {
 
     return (
         <div className="header-container flex flex-row justify-between w-full border-b-2 border-custom-purple">
-            <img src="https://docs.ufosc.org/img/logo.png" class="ml-4 w-12 h-12 items-left justify-left cursor-pointer" onClick={ () => window.open('https://ufosc.org/', '_blank')}/>
+
+            <img src="/BoL Logo Edited.png" className="logo" alt="BoL Logo" onClick={() => window.open('https://ufosc.org/', '_blank')}/>
+            <img src="https://docs.ufosc.org/img/logo.png" className="logo" alt="logo UFOSC Logo" onClick={() => window.open('https://ufosc.org/', '_blank')}/>
+           
             <div class="flex justify-end flex-grow items-center">
+            
             <div className="header-option-container" onClick={() => {navigate(`/`)}}>
                 <div className="header-option-text">Home</div>
             </div>
@@ -19,6 +23,7 @@ const Navbar = () => {
             <div className="header-option-container" onClick={() => {navigate(`/about`)}}>
                 <div className="header-option-text">About</div>
             </div>
+
             </div>
         </div>
     )
